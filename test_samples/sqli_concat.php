eb5717484d12c4d5bdd2509723c5105fd2f1d6a4
MasterUWA <MasterUWA@users.noreply.github.com>
2026-08-18 00:31:17 +0200

    Update project folder

--- test_samples/sqli_concat.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_GET['id'];
-$sql = "SELECT * FROM products WHERE id=" . $id;
-$result = mysqli_query($conn, $sql);
-
-while ($row = mysqli_fetch_assoc($result)) {
-    echo $row['name'] . "<br>";
-}
-?>
--- test_samples/xss_reflected.php
+++
@@ -1,9 +0,0 @@
-<?php
-$search = $_GET['search'];
-$message = $_POST['message'];
-$username = $_COOKIE['username'];
-
-echo "<h1>Search Results for: " . $search . "</h1>";
-echo "<p>Welcome back, " . $username . "</p>";
-echo "<div class='message'>" . $message . "</div>";
-?>
